27eccfa941bdb4819c9838075079e2c5a728af69
bbd <bbd.92@icloud.com>
2019-05-24 19:24:24 +0900

    add user list

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,28 @@
 $(function(){
+  function appendUsers(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    return html
+  };
+  function appendNoUser(){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">一致するユーザーが見つかりません</p>
+                </div>`
+    return html
+  };
+
+  function emptyList(){$('#user-search-result').empty();}
+
   $('#user-search-field').on('keyup', function(){
-    var input = $(this).val()
-    
+    var input = $(this).val();
+
+    if(input == ""){
+      emptyList()
+      return false
+    }
+
     $.ajax({
       type: "GET",
       url: '/users',
@@ -10,11 +31,18 @@ $(function(){
     })
 
     .done(function(users){
-      console.log(users)
+      emptyList()
+      if (users.length > 0){
+        users.forEach(function(user){
+          $('#user-search-result').append(appendUsers(user))
+        }
+      )} else {
+        $('#user-search-result').append(appendNoUser())
+      }
     })
 
-    .false(function(){
-      console.log("false")
+    .fail(function(){
+      alert('検索に失敗しました')
     })
   })
 });
