f9299a6f9ec79392ecb064b653d2dbbf39f6b911
bbd <bbd.92@icloud.com>
2019-05-26 14:31:44 +0900

    create index action

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.json {@messages = Message.where('id > ?' params[:last_id])}
   end
 end
